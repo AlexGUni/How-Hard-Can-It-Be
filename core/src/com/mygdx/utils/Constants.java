7b1dd81189d13707e8faa988f4c5422e44109152
AlexanderGhosh <ghoshalexander@gmail.com>
2021-12-04 04:22:17 +0000

    VIEWPORT constants factors in full screen or not

--- core/src/com/mygdx/utils/Constants.java
+++ core/src/com/mygdx/utils/Constants.java
@@ -1,17 +1,37 @@
 package com.mygdx.utils;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
 
 public final class Constants {
-    public final static float ASPECT_RATIO = 1.0f / 1.0f;
-    public final static int VIEWPORT_HEIGHT = 1080;
-    public final static int VIEWPORT_WIDTH = (int)(ASPECT_RATIO * VIEWPORT_HEIGHT);
-    public static final int HALF_VIEWPORT_HEIGHT = VIEWPORT_WIDTH / 2;
-    public static final int HALF_VIEWPORT_WIDTH = VIEWPORT_HEIGHT / 2;
-    public final static Vector2 DIMENTIONS = new Vector2(VIEWPORT_WIDTH, VIEWPORT_HEIGHT);
-    public final static Vector2 HALF_DIMENTIONS = new Vector2(HALF_VIEWPORT_WIDTH, HALF_VIEWPORT_HEIGHT);
-    public final static String VIEWPORT_TITLE = "Jimbo Mc.Fimbo";
 
-    public final static Vector3 BACKGROUND_COLOUR = new Vector3(0.5f, 0.5f, 0.5f);
+    public static void INIT_CONSTANTS() {
+        SCREEN_WIDTH = Gdx.graphics.getWidth();
+        SCREEN_HEIGHT = Gdx.graphics.getHeight();
+        FULLSCREEN = true;
+        ASPECT_RATIO = !FULLSCREEN ? 1.0f / 1.0f : (float)SCREEN_WIDTH / (float)SCREEN_HEIGHT;
+        VIEWPORT_HEIGHT = !FULLSCREEN ? 1080 : SCREEN_HEIGHT;
+        VIEWPORT_WIDTH = !FULLSCREEN ? (int)(ASPECT_RATIO * VIEWPORT_HEIGHT) : SCREEN_WIDTH;
+        HALF_VIEWPORT_HEIGHT = VIEWPORT_WIDTH / 2;
+        HALF_VIEWPORT_WIDTH = VIEWPORT_HEIGHT / 2;
+        DIMENTIONS = new Vector2(VIEWPORT_WIDTH, VIEWPORT_HEIGHT);
+        HALF_DIMENTIONS = new Vector2(HALF_VIEWPORT_WIDTH, HALF_VIEWPORT_HEIGHT);
+        VIEWPORT_TITLE = "Jimbo Mc.Fimbo";
+        BACKGROUND_COLOUR = new Vector3(0.5f, 0.5f, 0.5f);
+    }
+
+    public static int SCREEN_WIDTH;
+    public static int SCREEN_HEIGHT;
+    public static boolean FULLSCREEN;
+    public static float ASPECT_RATIO;
+    public static int VIEWPORT_HEIGHT;
+    public static int VIEWPORT_WIDTH;
+    public static int HALF_VIEWPORT_HEIGHT;
+    public static int HALF_VIEWPORT_WIDTH;
+    public static Vector2 DIMENTIONS;
+    public static Vector2 HALF_DIMENTIONS;
+    public static String VIEWPORT_TITLE;
+
+    public static Vector3 BACKGROUND_COLOUR;
 }
