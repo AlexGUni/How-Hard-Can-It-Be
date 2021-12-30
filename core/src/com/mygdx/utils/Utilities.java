205843c622792a6c729feecb31bcfe12366bff1b
AlexanderGhosh <ghoshalexander@gmail.com>
2021-12-30 16:57:40 +0000

    util funcs

--- core/src/com/mygdx/utils/Utilities.java
+++ core/src/com/mygdx/utils/Utilities.java
@@ -1,6 +1,9 @@
 package com.mygdx.utils;
 
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
+
+import static com.mygdx.utils.Constants.TILE_SIZE;
 
 public final class Utilities {
     public static float vectorToAngle (Vector2 v) {
@@ -12,16 +15,29 @@ public final class Utilities {
         return out;
     }
 
+    public static float tilesToDistance(float tiles){
+        return TILE_SIZE * tiles;
+    }
+
+    public static float angleBetween(Vector2 v, Vector2 w) {
+        return MathUtils.atan2(w.y * v.x - w.x * v.y, w.x * v.x + w.y * v.y);
+    }
+
+    public static float scale(float x, float min0, float max0, float min1, float max1) {
+        return (max1 - min1) * ((x - min0 * x) / (max0 * x - min0 * x)) + min1;
+    }
+
+    public static float scale(float x, Vector2 a, Vector2 b) {
+        return (b.y - b.x) * ((x - a.x * x) / (a.y * x - a.x * x)) + b.x;
+    }
+
     /**
-     * Doesn't affect 'a'
-     * @return returns an abs of 'a'
+     * @param x the vector to round
+     * @return x modified for chaining
      */
-    public static Vector2 abs(Vector2 a){
-        Vector2 res = a.cpy();
-
-        res.x = Math.abs(res.x);
-        res.y = Math.abs(res.y);
-
-        return res;
+    public static Vector2 round(Vector2 x) {
+        x.x = Math.round(x.x);
+        x.y = Math.round(x.y);
+        return x;
     }
 }
