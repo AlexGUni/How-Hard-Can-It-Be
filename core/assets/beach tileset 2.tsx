2d913ca15932bcc95b4a37c660c3355665d5940e
AlexanderGhosh <ghoshalexander@gmail.com>
2022-01-23 17:30:25 +0000

    cleanup

--- core/assets/beach tileset 2.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="beach tileset 2" tilewidth="32" tileheight="32" spacing="2" margin="1" tilecount="162" columns="18">
- <image source="Beach Tileset padded pink.png" width="612" height="306"/>
-</tileset>
