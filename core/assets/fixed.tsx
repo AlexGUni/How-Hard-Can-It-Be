4eb1a7862e928373df13ac99612106856d78a447
AlexanderGhosh <ghoshalexander@gmail.com>
2022-01-23 17:24:20 +0000

    map with padding

---
+++ core/assets/fixed.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="fixed" tilewidth="32" tileheight="32" spacing="4" margin="2" tilecount="117" columns="39">
+ <image source="Fix/fixed.png" width="1332" height="108"/>
+</tileset>
